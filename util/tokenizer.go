d6b9bfdd03e51fc9567c6a80db1903133d51cc14
Volodymyr Burenin <vburenin@gmail.com>
2015-09-02 12:04:44 -0700

    Reduced amount of code.

--- util/tokenizer.go
+++ util/tokenizer.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	STATE_NONE                 = 0
+	STATE_INIT                 = 0
 	STATE_PARSE_TEXT_TOKEN     = 1
 	STATE_PARSE_BINARY_HEADER  = 3
 	STATE_PARSE_BINARY_PAYLOAD = 4
@@ -30,13 +30,10 @@ var ERR_TOK_TOKEN_TOO_LONG = errors.New("Token is too long")
 var ERR_TOK_PARSING_ERROR = errors.New("Error during token parsing")
 
 type Tokenizer struct {
-	state       uint8
-	buffer      []byte
-	bufPos      int
-	bufLen      int
-	reader      io.Reader
-	binDataLeft int
-	curToken    []byte
+	buffer []byte
+	bufPos int
+	bufLen int
+	reader io.Reader
 }
 
 func UnsafeBytesToString(b []byte) string {
@@ -45,117 +42,79 @@ func UnsafeBytesToString(b []byte) string {
 
 func NewTokenizer(reader io.Reader) *Tokenizer {
 	tok := Tokenizer{
-		state:       STATE_NONE,
-		buffer:      make([]byte, MAX_RECV_BUFFER_SIZE),
-		bufPos:      0,
-		bufLen:      0,
-		binDataLeft: 0,
-		reader:      reader,
-		curToken:    nil,
+		buffer: make([]byte, MAX_RECV_BUFFER_SIZE),
+		bufPos: 0,
+		bufLen: 0,
+		reader: reader,
 	}
 	return &tok
-}
-
-func (tok *Tokenizer) appendToken(result []string) ([]string, error) {
-	if len(result) >= MAX_TOKENS_PER_MSG {
-		return nil, ERR_TOK_TOO_MANY_TOKENS
-	}
-	// Check for special tokens to handle their payload
-	if tok.state == STATE_PARSE_TEXT_TOKEN && tok.curToken[0] == '$' {
-		// Binary data marker
-		str := UnsafeBytesToString(tok.curToken[1:])
-		binLen, err := strconv.Atoi(str)
-		if err == nil && (binLen < 1 || binLen > MAX_BINARY_TOKEN_LEN) {
-			return result, ERR_TOK_PARSING_ERROR
-		}
-		tok.binDataLeft = binLen
-		tok.curToken = make([]byte, 0, tok.binDataLeft)
-		tok.state = STATE_PARSE_BINARY_PAYLOAD
-	} else {
-		result = append(result, UnsafeBytesToString(tok.curToken))
-		tok.curToken = nil
-		tok.state = STATE_PARSE_TEXT_TOKEN
-	}
-	return result, nil
-}
-
-func (tok *Tokenizer) resetTokenizer() {
-	tok.state = STATE_NONE
-	tok.bufPos = 0
-	tok.bufLen = 0
-	tok.curToken = nil
-}
-
-func (tok *Tokenizer) messageProcessed() {
-	tok.state = STATE_NONE
-	tok.curToken = nil
-	tok.bufPos += 1
 }
 
 func (tok *Tokenizer) ReadTokens() ([]string, error) {
 	result := make([]string, 0, MAX_TOKENS_PER_MSG)
 	var err error
+	var token []byte
+	var binTokenLen int
+	var state uint8 = STATE_INIT
+
 	for {
 		if tok.bufPos >= tok.bufLen {
 			// Read more data from the network reader
 			tok.bufPos = 0
 			tok.bufLen, err = tok.reader.Read(tok.buffer)
 			if nil != err {
-				tok.resetTokenizer()
 				return nil, err
 			}
 		}
 		// Tokenize content of the buffer
-		for tok.bufPos < tok.bufLen {
-			if tok.state == STATE_PARSE_BINARY_PAYLOAD {
-				availableBytes := tok.bufLen - tok.bufPos
-				if availableBytes > tok.binDataLeft {
-					availableBytes = tok.binDataLeft
-				}
-				tok.curToken = append(tok.curToken, tok.buffer[tok.bufPos:tok.bufPos+availableBytes]...)
-				tok.binDataLeft -= availableBytes
 
-				if tok.binDataLeft <= 0 {
-					// Token complete
-					result, err = tok.appendToken(result)
-					if err != nil {
-						tok.resetTokenizer()
-						return nil, err
-					}
+		for tok.bufPos < tok.bufLen {
+			if state == STATE_PARSE_BINARY_PAYLOAD {
+				availableBytes := tok.bufLen - tok.bufPos
+				if availableBytes > binTokenLen {
+					availableBytes = binTokenLen
+				}
+				token = append(token, tok.buffer[tok.bufPos:tok.bufPos+availableBytes]...)
+				binTokenLen -= availableBytes
+
+				if binTokenLen <= 0 {
+					// Binary token complete
+					result = append(result, UnsafeBytesToString(token))
+					state = STATE_INIT
 				}
 				tok.bufPos += availableBytes
 				continue
 			}
 
+			if state == STATE_INIT {
+				token = make([]byte, 0, 64)
+				state = STATE_PARSE_TEXT_TOKEN
+			}
+
 			val := tok.buffer[tok.bufPos]
 			tok.bufPos += 1
 
-			if val >= START_ASCII_RANGE && val <= END_ASCII_RANGE {
-				if tok.curToken == nil {
-					// Start parsing new text token
-					tok.state = STATE_PARSE_TEXT_TOKEN
-					tok.curToken = make([]byte, 0, 64)
-				}
-				if len(tok.curToken) < MAX_TEXT_TOKEN_LEN {
-					tok.curToken = append(tok.curToken, val)
+			if state == STATE_PARSE_TEXT_TOKEN {
+				if val >= START_ASCII_RANGE && val <= END_ASCII_RANGE {
+					token = append(token, val)
 				} else {
-					tok.resetTokenizer()
-					return nil, ERR_TOK_TOKEN_TOO_LONG
-				}
-			} else {
-				if tok.state == STATE_PARSE_TEXT_TOKEN && len(tok.curToken) > 0 {
-					// Current token completed. Add it to the result array and check for additional payload
-					// specific for this token
-					result, err = tok.appendToken(result)
-					if err != nil {
-						tok.resetTokenizer()
-						return nil, err
+					if len(token) > 0 {
+						if token[0] == '$' {
+							str := UnsafeBytesToString(token[1:])
+							binTokenLen, err = strconv.Atoi(str)
+							if err == nil && (binTokenLen < 1 || binTokenLen > MAX_BINARY_TOKEN_LEN) {
+								return nil, ERR_TOK_PARSING_ERROR
+							}
+							token = make([]byte, 0, binTokenLen)
+							state = STATE_PARSE_BINARY_PAYLOAD
+						} else {
+							result = append(result, UnsafeBytesToString(token))
+							state = STATE_INIT
+							if val == SYMBOL_CR {
+								return result, nil
+							}
+						}
 					}
-				}
-				if val == SYMBOL_CR {
-					// Wait for LF symbol following current CR symbol
-					tok.messageProcessed()
-					return result, nil
 				}
 			}
 		}
---
+++ util/tokenizer_test.go
@@ -0,0 +1,84 @@
+//package util_test
+//
+//import "testing"
+//import (
+//	"firempq/util"
+//	"fmt"
+//	"io"
+//)
+//
+//type TestDataReader struct {
+//	datas []string
+//	pos   int
+//}
+//
+//func (td *TestDataReader) Read(data []byte) (int, error) {
+//	if td.pos >= len(td.datas) {
+//		return 0, io.EOF
+//	}
+//	copy(data, td.datas[td.pos])
+//	td.pos++
+//	return len(td.datas[td.pos-1]), nil
+//}
+//
+//func TestReadTokens(t *testing.T) {
+//	var data []string
+//	//txt1 := "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+//	for i := 20; i > 0; i-- {
+//		txt := "0123456789"
+//		data = append(data, txt)
+//	}
+//	data = append(data, "\n fdsf\n")
+//	data = append(data, "$9 012345678 11 22\n")
+//	data = append(data, "$9 012345611 1 22\n")
+//
+//	r := &TestDataReader{data, 0}
+//	tok := util.NewTokenizer(r)
+//	for {
+//		val, err := tok.ReadTokens()
+//		if err == io.EOF {
+//			break
+//		}
+//		fmt.Println(val, len(val), len(val), err)
+//	}
+//
+//}
+
+package util_test
+
+import "testing"
+import (
+	"firempq/util"
+	"io"
+)
+
+type TestDataReader struct {
+	pos int
+}
+
+var TXT = "123123 12312312312 12312312312 12312312312 12312312312 asdasdasdas $10 0123456789\n"
+
+func (td *TestDataReader) Read(data []byte) (int, error) {
+	if td.pos > 1000000 {
+		return 0, io.EOF
+	}
+
+	copy(data, TXT)
+	td.pos += 1
+	return len(TXT), nil
+}
+
+func TestReadTokens(t *testing.T) {
+	println(len(TXT))
+	r := &TestDataReader{0}
+	startTs := util.Uts()
+	tok := util.NewTokenizer(r)
+	for {
+		_, err := tok.ReadTokens()
+		if err == io.EOF {
+			break
+		}
+	}
+	println(util.Uts() - startTs)
+
+}
