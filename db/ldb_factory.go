d2955da7138d126ea5fc94d5cc450214610485fd
Volodymyr Burenin <vburenin@gmail.com>
2015-12-05 18:11:19 -0800

    New database will be returned if current one is closed.

--- db/ldb_factory.go
+++ db/ldb_factory.go
@@ -3,6 +3,8 @@ package db
 import (
 	"os"
 	"sync"
+
+	"firempq/log"
 )
 
 var database *DataStorage
@@ -11,14 +13,22 @@ var lock sync.Mutex
 // GetDatabase returns DataStorage singleton.
 func GetDatabase() *DataStorage {
 	lock.Lock()
-	var err error
 	defer lock.Unlock()
+	return getDatabase()
+}
+
+func getDatabase() *DataStorage {
+	var err error
 	if database == nil {
 		database, err = NewDataStorage("databasedir")
 		if err != nil {
 			log.Error("Cannot initialize FireMPQ database: %s", err)
 			os.Exit(255)
 		}
+	}
+	if database.closed {
+		database = nil
+		return getDatabase()
 	}
 	return database
 }
