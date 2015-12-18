3dccf04c6fffd9ab300250b7154abb0d6510c52a
Volodymyr Burenin <vburenin@gmail.com>
2015-12-17 20:50:28 -0800

    Added function to compare multiple items at once.

--- testutils/itemresponse.go
+++ testutils/itemresponse.go
@@ -8,18 +8,18 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func VerifyItemsResponse(r IResponse, length int) ([]IItem, bool) {
+func VerifyItemsResponse(r IResponse, size int) ([]IItem, bool) {
 	ir, ok := r.(*common.ItemsResponse)
 	So(ok, ShouldBeTrue)
 	if ok {
 		items := ir.GetItems()
-		So(len(items), ShouldEqual, length)
-		return items, len(items) == length
+		So(len(items), ShouldEqual, size)
+		return items, len(items) == size
 	}
 	return nil, false
 }
 
-func VerifyItem(r IResponse, itemId, payload string) bool {
+func VerifySingleItem(r IResponse, itemId, payload string) bool {
 
 	if items, ok := VerifyItemsResponse(r, 1); ok {
 		So(items[0].GetId(), ShouldEqual, itemId)
@@ -29,17 +29,33 @@ func VerifyItem(r IResponse, itemId, payload string) bool {
 	return false
 }
 
-func VerifyOk(r IResponse) {
+func VerifyItems(r IResponse, size int, itemSpecs ...string) bool {
+	So(size*2, ShouldEqual, len(itemSpecs))
+	items, ok := VerifyItemsResponse(r, size)
+	if size*2 == len(itemSpecs) && ok {
+		for i := 0; i < len(itemSpecs); i += 2 {
+			itemPos := i / 2
+			itemId := itemSpecs[i]
+			itemPayload := itemSpecs[i+1]
+			So(items[itemPos].GetId(), ShouldEqual, itemId)
+			So(items[itemPos].GetPayload(), ShouldEqual, itemPayload)
+		}
+		return true
+	}
+	return false
+}
+
+func VerifyOkResponse(r IResponse) {
 	So(r, ShouldEqual, common.OK_RESPONSE)
 }
 
-func VerifyError(r IResponse) bool {
+func VerifyErrResponse(r IResponse) bool {
 	err := r.IsError()
 	So(err, ShouldBeTrue)
 	return err
 }
 
-func VerifySize(s ISvc, size int) bool {
+func VerifyServiceSize(s ISvc, size int) bool {
 	So(s.Size(), ShouldEqual, size)
 	return s.Size() == size
 }
