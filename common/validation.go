da96023dbd74cb8a47799da98b5fe6ee8b93d1eb
Volodymyr Burenin <vburenin@gmail.com>
2015-12-19 18:29:57 -0800

    Added item id validator code.

---
+++ common/validation.go
@@ -0,0 +1,28 @@
+package common
+
+// ValidateItemId checks if symbol range is in the allowed scope for the item id
+func ValidateItemId(itemId string) bool {
+	if len(itemId) == 0 {
+		return false
+	}
+	for _, itemId := range itemId {
+		if (itemId >= '0' && itemId <= '9') ||
+			(itemId >= 'a' && itemId <= 'z') ||
+			(itemId >= 'A' && itemId <= 'Z') ||
+			itemId == '_' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+// ValidateUserItemId checks if symbol range is in the allowed scope for the item id
+// User defined ID can not start with the underscore.
+func ValidateUserItemId(itemId string) bool {
+	if ValidateItemId(itemId) && itemId[0] != '_' {
+		return true
+	}
+	return false
+}
