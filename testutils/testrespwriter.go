d124e15528e948606b53b2a0f31f621fa24c1270
Volodymyr Burenin <vburenin@gmail.com>
2015-12-25 22:04:21 -0800

    Test Response writer.

---
+++ testutils/testrespwriter.go
@@ -0,0 +1,24 @@
+package testutils
+
+import (
+	. "firempq/api"
+	"sync"
+)
+
+type TestResponseWriter struct {
+	mutex     sync.Mutex
+	responses []IResponse
+}
+
+func (rw *TestResponseWriter) WriteResponse(resp IResponse) error {
+	rw.mutex.Lock()
+	rw.responses = append(rw.responses, resp)
+	rw.mutex.Unlock()
+	return nil
+}
+
+func NewTestResponseWriter() *TestResponseWriter {
+	return &TestResponseWriter{
+		responses: make([]IResponse, 0, 1000),
+	}
+}
