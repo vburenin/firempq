62226798dda53e5b7539c9fe426e694a060dc21f
Volodymyr Burenin <vburenin@gmail.com>
2015-12-27 14:29:46 -0800

    some new benchmark code.

--- benchmark/bench_pqueue.go
+++ benchmark/bench_pqueue.go
@@ -8,7 +8,10 @@ import (
 	. "firempq/api"
 	"firempq/db"
 	. "firempq/features/pqueue"
+	"firempq/structs/pheap"
 	. "firempq/testutils"
+	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -34,17 +37,18 @@ func BenchMassPush() {
 
 	ctx.Call(PQ_CMD_SET_PARAM, []string{CPRM_MAX_SIZE, "10000000", CPRM_MSG_TTL, "100"})
 
-	startTs := time.Now().UnixNano()
+	var grp sync.WaitGroup
 	data := []string{PRM_PAYLOAD, "7777777777777777777777777777777777777777777777777777777777777777"}
 
-	var grp sync.WaitGroup
 	testFunc := func() {
-		for i := 0; i < 1000000; i++ {
+		for i := 0; i < 100000; i++ {
 			ctx.Call(PQ_CMD_PUSH, data)
 		}
 		grp.Done()
 	}
 	log.Info("Adding goroutines")
+
+	startTs := time.Now().UnixNano()
 	for i := 0; i < 10; i++ {
 		grp.Add(1)
 		go testFunc()
@@ -52,14 +56,14 @@ func BenchMassPush() {
 	}
 	log.Info("Waiting for result")
 	grp.Wait()
+	finishTs := time.Now().UnixNano()
 	ctx.Finish()
 
-	finishTs := time.Now().UnixNano()
 	log.Info("Test finished in: %d - %d", (finishTs - startTs), (finishTs-startTs)/1000000)
-	db.GetDatabase().FlushCache()
-	f.DropService("BenchTest")
-	f.Close()
-	db.GetDatabase().Close()
+	//db.GetDatabase().FlushCache()
+	//f.DropService("BenchTest")
+	//f.Close()
+	//db.GetDatabase().Close()
 }
 
 func BenchMassPushMultiQueue() {
@@ -67,6 +71,7 @@ func BenchMassPushMultiQueue() {
 	resp1 := f.DropService("BenchTest1")
 	resp2 := f.DropService("BenchTest2")
 	resp3 := f.DropService("BenchTest3")
+	resp4 := f.DropService("BenchTest4")
 
 	resp1 = f.CreateService(common.STYPE_PRIORITY_QUEUE, "BenchTest1", []string{})
 	if resp1.IsError() {
@@ -83,6 +88,11 @@ func BenchMassPushMultiQueue() {
 		log.Fatal("Can not create BenchTest queue")
 	}
 
+	resp4 = f.CreateService(common.STYPE_PRIORITY_QUEUE, "BenchTest4", []string{})
+	if resp4.IsError() {
+		log.Fatal("Can not create BenchTest queue")
+	}
+
 	svc1, ok := f.GetService("BenchTest1")
 	if !ok {
 		log.Fatal("Could not get created service: BenchTest1")
@@ -95,16 +105,25 @@ func BenchMassPushMultiQueue() {
 	if !ok {
 		log.Fatal("Could not get created service: BenchTest3")
 	}
+
+	svc4, ok := f.GetService("BenchTest4")
+	if !ok {
+		log.Fatal("Could not get created service: BenchTest4")
+	}
+
 	respWriter1 := NewTestResponseWriter()
 	respWriter2 := NewTestResponseWriter()
 	respWriter3 := NewTestResponseWriter()
+	respWriter4 := NewTestResponseWriter()
 	ctx1 := svc1.NewContext(respWriter1)
 	ctx2 := svc2.NewContext(respWriter2)
 	ctx3 := svc3.NewContext(respWriter3)
+	ctx4 := svc4.NewContext(respWriter4)
 
 	ctx1.Call(PQ_CMD_SET_PARAM, []string{CPRM_MAX_SIZE, "10000000", CPRM_MSG_TTL, "100000", CPRM_DELIVERY_DELAY, "0"})
 	ctx2.Call(PQ_CMD_SET_PARAM, []string{CPRM_MAX_SIZE, "10000000", CPRM_MSG_TTL, "100000", CPRM_DELIVERY_DELAY, "0"})
 	ctx3.Call(PQ_CMD_SET_PARAM, []string{CPRM_MAX_SIZE, "10000000", CPRM_MSG_TTL, "100000", CPRM_DELIVERY_DELAY, "0"})
+	ctx4.Call(PQ_CMD_SET_PARAM, []string{CPRM_MAX_SIZE, "10000000", CPRM_MSG_TTL, "100000", CPRM_DELIVERY_DELAY, "0"})
 
 	startTs := time.Now().UnixNano()
 	data := []string{PRM_PAYLOAD, "7777777777777777777777777777777777777777777777777777777777777777"}
@@ -124,7 +143,8 @@ func BenchMassPushMultiQueue() {
 		go testFunc(ctx2)
 		grp.Add(1)
 		go testFunc(ctx3)
-
+		grp.Add(1)
+		go testFunc(ctx4)
 	}
 	log.Info("Waiting for result")
 	grp.Wait()
@@ -134,6 +154,9 @@ func BenchMassPushMultiQueue() {
 	svc2.Close()
 	ctx3.Finish()
 	svc3.Close()
+
+	ctx4.Finish()
+	svc4.Close()
 
 	finishTs := time.Now().UnixNano()
 	log.Info("Test finished in: %d - %d", (finishTs - startTs), (finishTs-startTs)/1000000)
@@ -146,6 +169,8 @@ func BenchMassPushMultiQueue() {
 	f.DropService("BenchTest2")
 	log.Info("Deleting BenchTest3")
 	f.DropService("BenchTest3")
+	log.Info("Deleting BenchTest4")
+	f.DropService("BenchTest4")
 	log.Info("Closing facade...")
 	f.Close()
 	log.Info("Waiting flush...")
@@ -153,8 +178,72 @@ func BenchMassPushMultiQueue() {
 	db.GetDatabase().Close()
 }
 
+func BenchHeap() {
+	println("Starting...")
+	st := time.Now().UnixNano()
+	s := 10000000
+	set := make(map[uint64]struct{})
+	dt := make([]uint64, 0, s)
+	rand.Seed(0)
+	for i := 0; i < s; i++ {
+		dt = append(dt, uint64(rand.Int63()))
+	}
+	for _, v := range dt {
+		set[v] = struct{}{}
+	}
+	if len(set) < s {
+		panic("Not enough data")
+	}
+	ft := time.Now().UnixNano() - st
+	fmt.Println("Data prepared in", float64(ft)/1000000, "ms")
+	println("Pushing into heap")
+	st = time.Now().UnixNano()
+	c := pheap.NewPHeap()
+	for idx, v := range dt {
+		c.Push(int64(idx%20), v)
+	}
+	ft = time.Now().UnixNano() - st
+	fmt.Println("Data prepared in", float64(ft)/1000000, "ms")
+}
+
+func sn2db(v uint64) string {
+
+	b := make([]byte, 8)
+	b[0] = uint8(v >> 56)
+	b[1] = uint8(v >> 48)
+	b[2] = uint8(v >> 40)
+	b[3] = uint8(v >> 32)
+	b[4] = uint8(v >> 24)
+	b[5] = uint8(v >> 16)
+	b[6] = uint8(v >> 8)
+	b[7] = uint8(v)
+	return common.UnsafeBytesToString(b)
+}
+
+func MarshalTest() {
+	//m := NewPQMsgMetaData("ereteteyeueueueueueueueu", 10, 123456789023, 1234)
+	st := time.Now().UnixNano()
+	var i uint64
+	for i = 0; i < 200000000; i++ {
+		b := make([]byte, 8)
+		b[0] = uint8(i >> 56)
+		b[1] = uint8(i >> 48)
+		b[2] = uint8(i >> 40)
+		b[3] = uint8(i >> 32)
+		b[4] = uint8(i >> 24)
+		b[5] = uint8(i >> 16)
+		b[6] = uint8(i >> 8)
+		b[7] = uint8(i)
+		common.UnsafeBytesToString(b)
+	}
+	ft := time.Now().UnixNano() - st
+	fmt.Println("Data prepared in", float64(ft)/1000000, "ms")
+}
+
 func main() {
 	log.InitLogging()
 	BenchMassPush()
+	//BenchHeap()
 	//BenchMassPushMultiQueue()
+	//MarshalTest()
 }
