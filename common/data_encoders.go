a1e6b430798e776ee91f7420e2af2a4e75079e61
Volodymyr Burenin <vburenin@gmail.com>
2015-12-27 18:15:33 -0800

    uint642bytes encoders.

--- common/data_encoders.go
+++ common/data_encoders.go
@@ -21,3 +21,27 @@ func EncodeRespString(data string) string {
 func EncodeRespInt64(val int64) string {
 	return ":" + strconv.FormatInt(val, 10)
 }
+
+func EncodeUint64ToString(v uint64) string {
+	b := make([]byte, 8)
+	b[0] = byte(v >> 56)
+	b[1] = byte(v >> 48)
+	b[2] = byte(v >> 40)
+	b[3] = byte(v >> 32)
+	b[4] = byte(v >> 24)
+	b[5] = byte(v >> 16)
+	b[6] = byte(v >> 8)
+	b[7] = byte(v)
+	return UnsafeBytesToString(b)
+}
+
+func DecodeBytesToUnit64(b []byte) uint64 {
+	return (uint64(b[7]) |
+		uint64(b[6])<<8 |
+		uint64(b[5])<<16 |
+		uint64(b[4])<<24 |
+		uint64(b[3])<<32 |
+		uint64(b[2])<<40 |
+		uint64(b[1])<<48 |
+		uint64(b[0])<<56)
+}
