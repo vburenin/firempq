8b354e8af807303532112946d5a7913fe73801d4
Volodymyr Burenin <vburenin@gmail.com>
2016-01-04 22:54:05 -0800

    Add flag to switch between goleveldb and levigo.

--- db/dbfactory.go
+++ db/dbfactory.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"firempq/db/cldb"
+	"firempq/db/ldb"
 	"firempq/log"
 	"os"
 	"sync"
@@ -11,6 +12,7 @@ import (
 
 var database DataStorage = nil
 var lock sync.Mutex
+var useGoLevelDB = false
 
 // GetDatabase returns DataStorage singleton.
 func GetDatabase() DataStorage {
@@ -26,7 +28,11 @@ func SetDatabase(ds DataStorage) {
 func getDatabase() DataStorage {
 	if database == nil {
 		var err error
-		database, err = cldb.NewLevelDBStorage("databasedir")
+		if useGoLevelDB {
+			database, err = ldb.NewLevelDBStorage("databasedir")
+		} else {
+			database, err = cldb.NewLevelDBStorage("databasedir")
+		}
 		if err != nil {
 			log.Error("Cannot initialize FireMPQ database: %s", err)
 			os.Exit(255)
