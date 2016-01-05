bb9930c954b8e1fff25c077aaecb8079188666a6
Volodymyr Burenin <vburenin@gmail.com>
2016-01-04 22:52:39 -0800

    Removed time related dead code.

--- common/timeutil.go
+++ common/timeutil.go
@@ -4,27 +4,6 @@ import (
 	"time"
 )
 
-func uts() int64 {
+func Uts() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
-}
-
-func UnixNanoTs() int64 {
-	return time.Now().UnixNano()
-}
-
-var testValue int64 = uts()
-var origUtc func() int64 = uts
-var Uts func() int64 = uts
-
-func IncTimer(inc int64) {
-	testValue += inc
-}
-
-func EnableTesting() {
-	testValue = Uts()
-	Uts = func() int64 { return testValue }
-}
-
-func DisableTesting() {
-	Uts = origUtc
 }
