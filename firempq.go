d59f9b6f16bcb8b51ac18c1fce664b94e760c8f4
Volodymyr Burenin <vburenin@gmail.com>
2016-07-31 16:33:58 -0700

    Embed profiler.

--- firempq.go
+++ firempq.go
@@ -4,10 +4,14 @@ import (
 	"github.com/vburenin/firempq/conf"
 	"github.com/vburenin/firempq/log"
 	"github.com/vburenin/firempq/server"
+
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
 	// Initialize logging to a default INFO level to be able to log config error.
+	go http.ListenAndServe(":5000", nil)
 	log.InitLogging()
 
 	err := conf.ReadConfig()
