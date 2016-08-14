4467b8dab97c394eeaf0bc8fb35b68435e0591a5
Volodymyr Burenin <vburenin@gmail.com>
2016-08-14 15:24:02 -0700

    Better config description

--- conf/config.go
+++ conf/config.go
@@ -15,12 +15,12 @@ type CLIParams struct {
 }
 
 type PQueueConfigData struct {
-	DefaultMessageTTL     int64 `long:"default-msg-ttl" description:"Default message TTL for a new queue in milliseconds" default:"3600000" env:"FMPQ_MSG_TTL"`
-	DefaultDeliveryDelay  int64 `long:"default-delivery-delay" description:"Default message delivery delay for a new queue in milliseconds" default:"0" env:"FMPQ_DELIVERY_DELAY"`
+	DefaultMessageTTL     int64 `long:"default-msg-ttl" description:"Default message TTL for a new queue in milliseconds" default:"3600000"`
+	DefaultDeliveryDelay  int64 `long:"default-delivery-delay" description:"Default message delivery delay for a new queue in milliseconds" default:"0"`
 	DefaultLockTimeout    int64 `long:"default-lock-timeout" description:"Default message lock/visibility timeout for a new queue in milliseconds" default:"60000"`
-	DefaultPopCountLimit  int64 `long:"default-pop-count-limit" description:"Default limit for a message receive attempts for a new queue" default:"99"`
+	DefaultPopCountLimit  int64 `long:"default-pop-count-limit" description:"Default receive attempts limit per message for a new queue" default:"99"`
 	DefaultMaxQueueSize   int64 `long:"default-max-queue-size" description:"Default max number of messages per queue" default:"100000000"`
-	DefaultPopWaitTimeout int64 `long:"default-wait-timeout" description:"Default wait timeout to receive a message from the queue for a new queue in milliseconds." default:"0"`
+	DefaultPopWaitTimeout int64 `long:"default-wait-timeout" description:"Default wait timeout to receive a message for a new queue in milliseconds." default:"0"`
 
 	MaxPopWaitTimeout int64 `long:"max-wait-timeout" description:"Limit receive wait timeout. Milliseconds." default:"20000"`
 	MaxPopBatchSize   int64 `long:"max-receive-batch" description:"Limit the number of received messages at once." default:"10"`
@@ -29,17 +29,14 @@ type PQueueConfigData struct {
 	MaxMessageTTL     int64 `long:"max-message-ttl" description:"Maximum message TTL for the queue. In milliseconds" default:"345600000"`
 	MaxMessageSize    int64 `long:"max-message-size" description:"Maximum message size in bytes." default:"262144"`
 
-	TimeoutCheckBatchSize int64 `long:"tune-process-batch" description:"Internal batching for expired and timedout messages. Large numbers may lead to not desired service timeouts" default:"1000"`
+	TimeoutCheckBatchSize int64 `long:"tune-process-batch" description:"Batch size to process expired messages and message locks. Large number may lead to not desired service timeouts" default:"1000"`
 }
 
 // Config is a generic service config type.
 type Config struct {
-	TextLogLevel        string `long:"log-level" description:"Log level" default:"info" choice:"debug" choice:"info" choice:"warning"`
-	Profiler            string `long:"profiler-address" description:"Enables Go profiler on the defined interface:port" default:""`
-	LogLevel            logging.Level
 	FMPQServerInterface string `long:"fmpq-address" description:"FireMPQ native protocol." default:":8222"`
 	SQSServerInterface  string `long:"sqs-address" description:"SQS protocol interface for FireMPQ" default:""`
-	SNSServerInterface  string `long:"sns-address" description:"NOT IMPLENTED: SNS protocol interface for FireMPQ" default:""`
+	SNSServerInterface  string `long:"sns-address" description:"NOT IMPLEMENTED: SNS protocol interface for FireMPQ" default:""`
 	DbFlushInterval     int64  `long:"flush-interval" description:"Disk synchronization interval in milliseconds" default:"100"`
 	DatabasePath        string `long:"data-dir" description:"FireMPQ database location" default:"fmpq-data"`
 	UpdateInterval      int64  `long:"update-interval" description:"Timeout and expiration check period in milliseconds" default:"100"`
@@ -51,7 +48,10 @@ type Config struct {
 
 	PQueueConfig PQueueConfigData
 
-	PrintConfig bool `long:"print-cfg" description:"Print current config values"`
+	TextLogLevel string `long:"log-level" description:"Log level" default:"info" choice:"debug" choice:"info" choice:"warning"`
+	PrintConfig  bool   `long:"print-cfg" description:"Print current config values"`
+	Profiler     string `long:"profiler-address" description:"Enables Go profiler on the defined interface:port" default:""`
+	LogLevel     logging.Level
 }
 
 var CFG *Config
@@ -62,7 +62,6 @@ func ParseConfigParameters() *Config {
 
 	_, err := flags.Parse(&cfg)
 	if err != nil {
-		log.Printf("Could not parse CLI parameters: %v", err)
 		os.Exit(255)
 	}
 
